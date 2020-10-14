971c1261b34b9d697dd579579acfedbdc0a0b54f
sergio reyes <sreyes@agsidegua.com.gt>
2020-10-13 21:06:56 -0600

    fin de la clase notas alumnos

--- src/ejerciciosdenotas/Ejerciciosdenotas.java
+++ src/ejerciciosdenotas/Ejerciciosdenotas.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 
 /**
  *Bryner Bonilla 2000233
+ * Sergio Reyes 2000061
  * @author Josué Tejeda 2000256
  */
 public class Ejerciciosdenotas {
@@ -64,6 +65,79 @@ public static  Scanner sc = new Scanner (System.in);
                     }
         
     }
-
+    public static void IngresodeDatos(){
+        System.out.println("Ingresa Carne");
+                    Carne= sc.nextInt();
+                    System.out.println("Ingresa Nombre");
+                    nombre = sc.next();
+                    System.out.println("Ingresa Parcial Uno");
+                    parcialUno = sc.nextDouble();
+                    System.out.println("Ingresa Parcial Dos");
+                    parcialDos = sc.nextDouble();
+                    System.out.println("Ingresa Zona");
+                    zona = sc.nextDouble();
+                    System.out.println("Ingresa Examen Final");
+                    examenFinal = sc.nextDouble();
+                    
+                    ListAlumnos.add(new Alumnos (Carne, nombre, parcialUno, parcialDos, zona, examenFinal));
+                    
     }
+  public static void CambiarDatos(){
+      int cambio;
+                    int indice =0;
+                    System.out.println("Cual es el NO.Carne");
+                    cambio = sc.nextInt();
+                    for (Alumnos alumno: ListAlumnos) {
+                        if(alumno.getCarne()== cambio){
+                            int seleccionar;
+                            System.out.println("Seleccione otra opcion");
+                            System.out.println("Opcion 1, cambiar Nombre");
+                            System.out.println("Opcion 2, cambiar Parcial Uno");
+                            System.out.println("Opcion 3, cambiar Parcial Dos");
+                            System.out.println("Opcion 4, cambiar zona");
+                            System.out.println("Opcion 5, cambiar examenFinal");
+                            seleccionar = sc.nextInt();
+                            switch(seleccionar){
+                                
+                                case 1:
+                                    System.out.println("Cambiar Nombre");
+                                    nombre = sc.next();
+                                    ListAlumnos.get(indice).setNombre(nombre);
+                                    break;
+                                case 2:
+                                    System.out.println("Cambiar parcialUno");
+                                    parcialUno = sc.nextDouble();
+                                   ListAlumnos.get(indice).setParcialUno(parcialUno);
+                                   ListAlumnos.get(indice).setTotal(alumno.getParcialUno()+ alumno.getParcialDos()+alumno.getZona()+ alumno.getExamenFinal());
+                                    break;
+                                case 3:
+                                    System.out.println("Cambiar parcialDos");
+                                    parcialDos = sc.nextDouble();
+                                    ListAlumnos.get(indice).setParcialDos(parcialDos);
+                                    ListAlumnos.get(indice).setTotal(alumno.getParcialUno()+alumno.getParcialDos()+alumno.getZona()+alumno.getExamenFinal());
+                                    break;
+                                case 4:
+                                    System.out.println("Cambiar zona");
+                                    zona = sc.nextDouble();
+                                    ListAlumnos.get(indice).setZona(zona);
+                                    ListAlumnos.get(indice).setTotal(alumno.getParcialUno()+alumno.getParcialDos()+alumno.getZona()+alumno.getExamenFinal());
+                                    break;
+                                case 5:
+                                    System.out.println("Cambiar Examen final");
+                                    examenFinal = sc.nextDouble();
+                                    ListAlumnos.get(indice).setExamenFinal(examenFinal);
+                                    ListAlumnos.get(indice).setTotal(alumno.getParcialUno()+alumno.getParcialDos()+alumno.getZona()+alumno.getExamenFinal());
+
+                                    break;      
+                            }
+                        }
+                        indice++;
+                    }
+
+  }
+  
+                        
+}
+
+    
         
